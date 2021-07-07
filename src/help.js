7abc48d3a0f31fc859dd82effe9e191d31a12885
TsukasaChanKawaiii <86638170+TsukasaChanKawaiii@users.noreply.github.com>
2021-07-07 21:05:06 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,51 @@
+const help = (prefix) => {
+	return `┏━━⬣  𝕀ℕ𝔽𝕆 [ √ ]
+┃⬡ [ Baileys ] Server
+┃⬡ [ SELF ] MODE
+┃⬡ [ Vivo ] HANDPHONE
+┗━━⬣ <\ 𝕿𝖘𝖚𝖐𝖆𝖘𝖆 𝕮𝖍𝖆𝖓 ، 々ʹʹ
+
+┏━━⬣  𝔹𝕌𝔾 𝕎𝔸 [ √ ]
+┃⬡ BUG KATALOG
+┃⬡ BUG IMAGE
+┃⬡ BUG GIF
+┃⬡ BUG DOKUMEN
+┃⬡ BUG LOC
+┃⬡ BUG VN
+┃⬡ BUG KONTAK
+┃⬡ BUG STICKER
+┃⬡ BUG GC
+┃⬡ VIRTEX
+┗━━⬣ <\ 𝕿𝖘𝖚𝖐𝖆𝖘𝖆 𝕮𝖍𝖆𝖓 ، 々ʹ⁩
+
+┏━━⬣  𝕊𝕋𝕆ℝ𝔸𝔾𝔼 [ √ ]
+┃⬡ ADD IMG
+┃⬡ LIST IMG 
+┃⬡ GET IMG
+┃⬡ ADD VN
+┃⬡ LIST VN
+┃⬡ GET VN
+┃⬡ ADD STIK
+┃⬡ LIST STIK
+┃⬡ GET STIK
+┃⬡ ADD VID
+┃⬡ LIST VID
+┃⬡ GET VID
+┗━━⬣ <\ 𝕿𝖘𝖚𝖐𝖆𝖘𝖆 𝕮𝖍𝖆𝖓 ، 々ʹ⁩
+
+┏━━⬣ 𝕊𝕆𝕊𝕀𝔸𝕃 𝕄𝔼𝔻𝕀𝔸 [ √ ]
+┃⬡ GH : https://github.com/TsukasaChanKawaiii
+┃⬡ FB : https://www.facebook.com/profile.php?id=100070153536766
+┗━━⬣ <\ 𝕿𝖘𝖚𝖐𝖆𝖘𝖆 𝕮𝖍𝖆𝖓 ، 々ʹʹ
+
+┏━━⬣  𝕆𝕋ℍ𝔼ℝ𝕊 [ √ ]
+┃⬡ SC ORI MhankBarBar
+┃⬡ RECODE BY TSUKASA CHAN 
+┃⬡ SENKUY [ √ ]
+┃⬡ FAJAR 404 [ √ ]
+┗━━⬣ <\ 𝕿𝖘𝖚𝖐𝖆𝖘𝖆 𝕮𝖍𝖆𝖓 ، 々ʹ⁩
+
+NOTE : Fitur Yang Lain Eror, Males Nge Fix Mager & Ga Niat :/`
+}
+
+exports.help = help
